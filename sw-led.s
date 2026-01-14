9b10fc64448c0e888e06fe1b5b03c12d0c3296e2
Eric Wustrow <ewust@colorado.edu>
2026-01-13 21:05:23 -0700

    more consistent

--- sw-led.s
+++ sw-led.s
@@ -1,13 +1,16 @@
 .text
+    .equ    LED_ADDR,   0xFF200000
+    .equ    SW_ADDR,    0xFF200040
+    .global _start
 _start:
-    movia   r4, 0xFF200000   # LED addr
-    movia   r5, 0xFF200040   # SW addr
+    movia   r2, LED_ADDR
+    movia   r3, SW_ADDR
 
 loop:
-    ldwio   r6, 0(r5)
+    ldwio   r4, 0(r3)
     
     # Your code here
 
-    stwio   r6, 0(r4)
+    stwio   r4, 0(r2)
     br      loop
 
